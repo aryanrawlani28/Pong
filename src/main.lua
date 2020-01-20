2264e02ab4a01d991e8e8b4b0a19c1c3f25af284
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-21 00:10:10 +0530

    Retroactive filtering added

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,47 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1366 
 WINDOW_HEIGHT = 768
 
+VIRTUAL_HEIGHT = 640
+VIRTUAL_WIDTH = 480
+
 -- Startup function
 function love.load()
-    love.window.setMode(
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    vsync = true,
+    resizable = false
+    })
+
+    --[[
+        love.window.setMode(
         WINDOW_WIDTH, WINDOW_HEIGHT, {
-            fullscreen = false,
-            vsync = true,
-            resizable = false
         }
     )
+    ]]--
 end
 
 -- Called after updates
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
         'Hello Pong!', -- Text
         0,             -- X Coord
-        WINDOW_HEIGHT / 2, -- Y Coord
-        WINDOW_WIDTH,          -- Width
-        'center')              -- Alignment
+        VIRTUAL_HEIGHT / 2, -- Y Coord
+        VIRTUAL_WIDTH,          -- Width
+        'center'               -- Alignment
+    )              
+
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
